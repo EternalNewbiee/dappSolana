363b094e3ae419754b78176d2ddf91abd9f2cb6f
EternalNewbiee <secuya.francis.michael@gmail.com>
2023-11-12 06:12:13 +0800

    Darker Color Scheme

--- app/hooks/todo.js
+++ app/hooks/todo.js
@@ -1,134 +1,150 @@
-import * as anchor from '@project-serum/anchor'
-import { useEffect, useMemo, useState } from 'react'
-import { TODO_PROGRAM_PUBKEY } from '../constants'
-import todoIDL from '../constants/todo.json'
-import toast from 'react-hot-toast'
-import { SystemProgram } from '@solana/web3.js'
-import { utf8 } from '@project-serum/anchor/dist/cjs/utils/bytes'
-import { findProgramAddressSync } from '@project-serum/anchor/dist/cjs/utils/pubkey'
-import { useAnchorWallet, useConnection, useWallet } from '@solana/wallet-adapter-react'
-import { authorFilter } from '../utils'
-
+import * as anchor from '@project-serum/anchor';
+import { useEffect, useMemo, useState } from 'react';
+import { TODO_PROGRAM_PUBKEY } from '../constants';
+import todoIDL from '../constants/todo.json';
+import toast from 'react-hot-toast';
+import { SystemProgram } from '@solana/web3.js';
+import { utf8 } from '@project-serum/anchor/dist/cjs/utils/bytes';
+import { findProgramAddressSync } from '@project-serum/anchor/dist/cjs/utils/pubkey';
+import { useAnchorWallet, useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { authorFilter } from '../utils';
 
 export function useTodo() {
-    const { connection } = useConnection()
-    const { publicKey } = useWallet()
-    const anchorWallet = useAnchorWallet()
+    const { connection } = useConnection();
+    const { publicKey } = useWallet();
+    const anchorWallet = useAnchorWallet();
 
-    const [initialized, setInitialized] = useState(false)
-    const [lastTodo, setLastTodo] = useState(0)
-    const [todos, setTodos] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [transactionPending, setTransactionPending] = useState(false)
-    const [input, setInput] = useState("")
-
+    const [initialized, setInitialized] = useState(false);
+    const [lastTodo, setLastTodo] = useState(0);
+    const [todos, setTodos] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [transactionPending, setTransactionPending] = useState(false);
+    const [input, setInput] = useState('');
 
     const program = useMemo(() => {
         if (anchorWallet) {
-            const provider = new anchor.AnchorProvider(connection, anchorWallet, anchor.AnchorProvider.defaultOptions())
-            return new anchor.Program(todoIDL, TODO_PROGRAM_PUBKEY, provider)
+            const provider = new anchor.AnchorProvider(connection, anchorWallet, anchor.AnchorProvider.defaultOptions());
+            return new anchor.Program(todoIDL, TODO_PROGRAM_PUBKEY, provider);
         }
-    }, [connection, anchorWallet])
+    }, [connection, anchorWallet]);
 
     useEffect(() => {
-    
         const findProfileAccounts = async () => {
-            if(program && publicKey && !transactionPending){
+            if (program && publicKey && !transactionPending) {
                 try {
-                    setLoading(true)
-                    const [profilePda, profileBump] = await findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId)
-                    const profileAccount = await program.account.userProfile.fetch(profilePda)
+                    setLoading(true);
+                    const [profilePda, profileBump] = await findProgramAddressSync(
+                        [utf8.encode('USER_STATE'), publicKey.toBuffer()],
+                        program.programId
+                    );
+                    const profileAccount = await program.account.userProfile.fetch(profilePda);
 
-                    if(profileAccount) {
-                        setLastTodo(profileAccount.lastTodo)
-                        setInitialized(true)
+                    if (profileAccount) {
+                        setLastTodo(profileAccount.lastTodo);
+                        setInitialized(true);
 
-                        const todoAccounts = await program.account.todoAccount.all([authorFilter(publicKey.toString())])
-                        setTodos(todoAccounts)
-                        console.log("Initialized!")
+                        const todoAccounts = await program.account.todoAccount.all([authorFilter(publicKey.toString())]);
+                        setTodos(todoAccounts);
+                        console.log('Initialized!');
                     } else {
-                        console.log("Not yet initialized.")
-                        setInitialized(false)
+                        console.log('Not yet initialized.');
+                        setInitialized(false);
                     }
                 } catch (error) {
-                    console.log(error)
-                    setInitialized(false)
-                    setTodos([])
+                    console.log(error);
+                    setInitialized(false);
+                    setTodos([]);
                 } finally {
-                    setLoading(false)
+                    setLoading(false);
                 }
             }
-        }
+        };
 
-        findProfileAccounts()
+        findProfileAccounts();
+    }, [publicKey, program, transactionPending]);
 
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    };
 
-    }, [publicKey, program, transactionPending])
-
-    const handleChange = (e)=> {
-        setInput(e.target.value)
-    }
-  
     const initializeUser = async () => {
-        if(program && publicKey) {
+        if (program && publicKey) {
             try {
-                setTransactionPending(true)
-                const [profilePda, profileBump] = findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId)
+                setTransactionPending(true);
+                const [profilePda, profileBump] = findProgramAddressSync(
+                    [utf8.encode('USER_STATE'), publicKey.toBuffer()],
+                    program.programId
+                );
 
                 const tx = await program.methods
-                .inititalizeUser()
-                .accounts({
-                    userProfile: profilePda,
-                    authority: publicKey,
-                    SystemProgram: SystemProgram.programId
-                })
-                .rpc()
+                    .inititalizeUser()
+                    .accounts({
+                        userProfile: profilePda,
+                        authority: publicKey,
+                        SystemProgram: SystemProgram.programId,
+                    })
+                    .rpc();
 
-                setInitialized(true)
-                toast.success('Successfully Initialized')
-
+                setInitialized(true);
+                toast.success('Successfully Initialized');
             } catch (error) {
-                console.log(error)
-                toast.error(error.toString)
+                console.log(error);
+                toast.error(error.toString);
             } finally {
-                setTransactionPending(false)
-            } 
+                setTransactionPending(false);
+            }
         }
-    }
+    };
 
     const addTodo = async (e) => {
-        e.preventDefault()
-        if(program && publicKey){
+        e.preventDefault();
+        if (program && publicKey) {
             try {
-                setTransactionPending(true)
-                const [profilePda, profileBump] = findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId)
-                const [todoPda, todoBump] = findProgramAddressSync([utf8.encode('TODO_STATE'), publicKey.toBuffer(), Uint8Array.from([lastTodo])], program.programId)
+                setTransactionPending(true);
+                const [profilePda, profileBump] = findProgramAddressSync(
+                    [utf8.encode('USER_STATE'), publicKey.toBuffer()],
+                    program.programId
+                );
+                const [todoPda, todoBump] = findProgramAddressSync(
+                    [utf8.encode('TODO_STATE'), publicKey.toBuffer(), Uint8Array.from([lastTodo])],
+                    program.programId
+                );
 
-                if(input) {
-                    console.log(publicKey.toString())
-                    console.log(lastTodo)
+                if (input) {
+                    console.log(publicKey.toString());
+                    console.log(lastTodo);
                     await program.methods
                         .addTodo(input)
                         .accounts({
                             userProfile: profilePda,
                             todoAccount: todoPda,
                             authority: publicKey,
-                            SystemProgram: SystemProgram.programId
+                            SystemProgram: SystemProgram.programId,
                         })
-                        .rpc()
-                    toast.success("Successfully added todo!")
+                        .rpc();
+                    toast.success('Successfully added todo!');
                 }
             } catch (error) {
-                console.log(error)
-                toast.error(error.toString())
+                console.log(error);
+                toast.error(error.toString());
             } finally {
-                setTransactionPending(false)
-                setInput("")
+                setTransactionPending(false);
+                setInput('');
             }
         }
-    }
+    };
 
-    const incompleteTodos = useMemo(() => todos.filter((todo) => !todo.account.marked), [todos])
+    const incompleteTodos = useMemo(() => todos.filter((todo) => !todo.account.marked), [todos]);
 
-    return { initialized, loading, transactionPending, incompleteTodos, input, setInput, handleChange, initializeUser, addTodo }
+    return {
+        initialized,
+        loading,
+        transactionPending,
+        incompleteTodos,
+        input,
+        setInput,
+        handleChange,
+        initializeUser,
+        addTodo,
+    };
 }
--- app/pages/index.js
+++ app/pages/index.js
@@ -4,41 +4,59 @@ import Loading from '../components/Loading';
 import TodoSection from '../components/todo/TodoSection';
 import styles from '../styles/Home.module.css';
 
-
 const Home = () => {
-    const { initialized, loading, transactionPending, incompleteTodos, addTodo, input,  handleChange, initializeUser } = useTodo()
+    const {
+        initialized,
+        loading,
+        transactionPending,
+        incompleteTodos,
+        addTodo,
+        input,
+        handleChange,
+        initializeUser
+    } = useTodo();
 
+    const renderTodoInput = () => (
+        <div className={styles.todoInput}>
+            <div className={styles.inputContainer}>
+                <form onSubmit={addTodo}>
+                    <input
+                        value={input}
+                        onChange={handleChange}
+                        id={styles.inputField}
+                        type="text"
+                        placeholder="Write your newest Victim"
+                    />
+                </form>
+            </div>
+        </div>
+    );
+
+    const renderInitializeButton = () => (
+        <button
+            type="button"
+            className={styles.button}
+            onClick={() => initializeUser()}
+            disabled={transactionPending}
+        >
+            Initialize
+        </button>
+    );
 
     return (
         <div className={styles.container}>
             <div className={styles.actionsContainer}>
-                {initialized ? (
-                    <div className={styles.todoInput}>
-                        <div className={styles.inputContainer}>
-                            <form onSubmit={addTodo}>
-                                <input value = {input} onChange={handleChange} id={styles.inputField} type="text" placeholder='Write your newest Victim' />
-                            </form>
-                        </div>
-                        <div className={styles.iconContainer}>
-       
-                        </div>
-                    </div>
-                ) : (
-                    <button type="button" className={styles.button} onClick={() => initializeUser()} disabled={transactionPending}>
-                        Initialize
-                    </button>
-                )}
-
-                <WalletMultiButton/>
+                {initialized ? renderTodoInput() : renderInitializeButton()}
+                <WalletMultiButton />
             </div>
 
             <div className={styles.mainContainer}>
                 <Loading loading={loading}>
-                    <TodoSection title="Victims" todos={incompleteTodos}/>
+                    <TodoSection title="Victims" todos={incompleteTodos} />
                 </Loading>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
